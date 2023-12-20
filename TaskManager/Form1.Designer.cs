c4394087fcc7bfe40996358cbe9a378720129455
ADMIN <ADMIN@DESKTOP-CBRV7QC>
2023-12-20 21:07:55 +0500

    Добавьте файлы проекта.

---
+++ TaskManager/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TaskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.TimeUpdateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(800, 450);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TimeUpdateToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(180, 26);
+            // 
+            // TimeUpdateToolStripMenuItem
+            // 
+            this.TimeUpdateToolStripMenuItem.Name = "TimeUpdateToolStripMenuItem";
+            this.TimeUpdateToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.TimeUpdateToolStripMenuItem.Text = "Время обновления";
+            this.TimeUpdateToolStripMenuItem.Click += new System.EventHandler(this.TimeUpdateToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem TimeUpdateToolStripMenuItem;
+    }
+}
+
---
+++ TaskManager/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskManager
+{
+    public  partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            DataColumn idcolumn = new DataColumn("Id",Type.GetType("System.Int32"));
+            DataColumn namecolumn = new DataColumn("Name", Type.GetType("System.String"));
+            DataColumn timestart = new DataColumn("TimeStart", Type.GetType("System.String"));
+            DataColumn timeprocess = new DataColumn("TimeProcess", Type.GetType("System.String"));
+            DataColumn countthread = new DataColumn("CountThread", Type.GetType("System.Int32"));
+            dt.Columns.Add(idcolumn);
+            dt.Columns.Add(namecolumn);
+            dt.Columns.Add(timestart);
+            dt.Columns.Add(timeprocess);
+            dt.Columns.Add(countthread);
+            ////dgMainFill(dataGridView1);
+            reloadTime = 1000;
+            timer1 = new System.Threading.Timer(dgMainFill, dataGridView1, reloadTime, reloadTime);
+    
+        }
+        private static DataTable dt = new DataTable();
+        private static Process[] processes;
+        public static Int32 reloadTime;
+        System.Threading.Timer timer1;
+      
+        public static void dgMainFill(object sender)
+        {
+            processes = Process.GetProcesses();
+            foreach(Process process in processes)
+            { string s,t;
+                try { s = process.StartTime.ToString(); } catch { s = ""; }
+                try {  t = process.TotalProcessorTime.ToString(); } catch { t = ""; }
+                try { dt.Rows.Add(new object[] { process.Id, process.ProcessName, s, t, process.Threads.Count }); } catch { }
+            }
+             (sender as DataGridView).Invoke((MethodInvoker)delegate
+            {
+                (sender as DataGridView).DataSource = dt;
+            });
+        }
+
+        private void TimeUpdateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Hello!!");
+            Form form = new Form2();
+            form.ShowDialog();
+            timer1.Change(reloadTime, reloadTime);
+        }
+    }
+}
